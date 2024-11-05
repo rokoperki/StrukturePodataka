86195126150f2affa71d8439af64fea7551de4dc
rokoperki <roko.perisic@gmail.com>
2024-11-05 15:03:39 +0100

    third labs

--- vjezbe2/zad2.c
+++ vjezbe2/zad2.c
@@ -16,52 +16,48 @@ positon create_person(char* fname, char* lname, int birth_year);
 int prepend_list(positon head, char* fname, char* lname, int birth_year);
 int append_list(positon head, char* fname, char* lname, int birth_year);
 int print_list(positon first);
-int insert_after(positon previous, positon to_insert);
 positon find_last(positon head);
 positon find_by_lname(positon first, char* lname);
 int delete_person(positon head, positon to_delete);
-int insert_before(positon next, positon to_insert, positon head);
-int sort_by_lname(positon head);
-int save_list_to_file(positon first, char* filename);
-int read_list_from_file(positon first, char* filename);
+int insert_after(positon after, positon new_person);
+positon find_before(positon head, char* lname);
+int insert_before(char* lname, positon new_person, positon head);
+int save_to_file(positon head, char* filename);
+int read_from_file(positon head, char* filename);
 
 int main()
 {
     person head = {.fname = "", .lname = "", .birth_year = 0, .next = NULL};
-    prepend_list(&head, "Jane", "Doe", 1990);
-    prepend_list(&head, "John", "Janice", 1991);
-    append_list(&head, "Jack", "Jacky", 1992);
+    prepend_list(&head, "Mia", "Mijic", 1990);
+    prepend_list(&head, "Jure", "Juric", 1991);
+    append_list(&head, "Ivo", "Ivic", 1992);
     print_list(&head);
 
-    positon jane = find_by_lname(&head, "Doe");
-    printf("Found: %s %s\n", jane->fname, jane->lname);
+    positon jure = find_by_lname(&head, "Juric");
+    printf("Found: %s %s\n", jure->fname, jure->lname);
 
-    printf("Deleting Jane\n");
-    delete_person(&head, jane);
+    printf("Deleting Jure\n");
+    delete_person(&head, jure);
     print_list(&head);
 
-    printf("Inserting new person after Jack\n");
-    positon jack = find_by_lname(&head, "Jacky");
-    positon new_person = create_person("Jill", "Doe", 1993);
-    insert_after(jack, new_person);
-    print_list(&head);
-    
-    printf("Inserting new person before Jack\n");
-    new_person = create_person("Mate", "Mates", 1993);
-    insert_before(jack, new_person, &head);
+    printf("Inserting Mate after Mia\n");
+    positon mia = find_by_lname(&head, "Mijic");
+    positon mate = create_person("Mate", "Matic", 1990);
+    insert_after(mia, mate);
     print_list(&head);
 
-    printf("Sorting by last name\n");
-    sort_by_lname(&head);
+    positon beforeIvo = find_before(&head, "Ivic");
+    positon luka = create_person("Luka", "Lukic", 1993);
+    printf("Inserting Luka before Ivo\n");
+    insert_before("Ivic", luka, &head);
     print_list(&head);
 
-    printf("Saving list to file\n");
-    save_list_to_file(&head, "zad2.txt");
+    save_to_file(&head, "zad2.txt");
 
-    person headFile = {.fname = "", .lname = "", .birth_year = 0, .next = NULL};
-    read_list_from_file(&headFile, "zad2.txt");
-    printf("Reading list from file\n");
-    print_list(&headFile);
+    head.next = NULL;
+    printf("Reading from file\n");
+    read_from_file(&head, "zad2a.txt");
+    print_list(&head);
 
     return 0;
 }
@@ -169,49 +165,46 @@ int delete_person(positon head, positon to_delete)
     return 0;
 }
 
-int insert_after(positon previous, positon to_insert)
+int insert_after(positon after, positon new_person)
 {
-    to_insert->next = previous->next;
-    previous->next = to_insert;
+    new_person->next = after->next;
+    after->next = new_person;
 
     return 0;
 }
 
-int insert_before(positon next, positon to_insert, positon head)
+positon find_before(positon head, char* lname)
 {
     positon temp = head;
-    while (temp->next != next)
+
+    while (temp->next)
     {
+        if (strcmp(temp->next->lname, lname) == 0)
+        {
+            return temp;
+        }
         temp = temp->next;
     }
 
-    temp->next = to_insert;
-    to_insert->next = next;
-
-    return 0;
+    return NULL;
 }
 
-int sort_by_lname(positon head)
+int insert_before(char* lname, positon new_person, positon head)
 {
-    positon i, j;
-    for (i = head->next; i->next; i = i->next)
+    positon temp = find_before(head, lname);
+    if (!temp)
     {
-        for (j = i->next; j; j = j->next)
-        {
-            if (strcmp(i->lname, j->lname) > 0)
-            {
-                char temp[32];
-                strcpy(temp, i->lname);
-                strcpy(i->lname, j->lname);
-                strcpy(j->lname, temp);
-            }
-        }
+        printf("Person before last name %s not found\n", lname);
+        return -1;
     }
 
+    new_person->next = temp->next;
+    temp->next = new_person;
+
     return 0;
 }
 
-int save_list_to_file(positon head, char* filename)
+int save_to_file(positon head, char* filename)
 {
     FILE* file = fopen(filename, "w");
     if (!file)
@@ -219,35 +212,60 @@ int save_list_to_file(positon head, char* filename)
         printf("Failed to open file %s\n", filename);
         return -1;
     }
-
     positon temp = head->next;
     while (temp)
     {
         fprintf(file, "%s %s %d\n", temp->fname, temp->lname, temp->birth_year);
         temp = temp->next;
     }
-
-    printf("List saved to file %s\n", filename);
+    printf("Data saved to file %s\n", filename);
     fclose(file);
-    return 0;   
+ 
+    return 0;
 }
 
-int read_list_from_file(positon head, char* filename)
-{
+int read_from_file(positon head, char* filename){
     FILE* file = fopen(filename, "r");
     if (!file)
     {
         printf("Failed to open file %s\n", filename);
         return -1;
     }
-
-    char fname[32], lname[32];
-    int birth_year;
-    while (fscanf(file, "%s %s %d", fname, lname, &birth_year) == 3)
+    char BUFFER[150];
+    while (fgets(BUFFER, sizeof(BUFFER), file))
     {
+        char fname[32], lname[32];
+        int birth_year;
+        sscanf(BUFFER, "%s %s %d", fname, lname, &birth_year);
         append_list(head, fname, lname, birth_year);
     }
 
-    fclose(file);
     return 0;
 }
+
+/*void sort_by_lname(positon head){
+    positon current = head;
+    bool swapped = true;
+
+    if(!current->next || !current->next->next){
+        return;
+    }
+
+    while (swapped)
+    {
+       current = head;
+       swapped = false;
+       while (current->next->next)
+       {
+            if (strcmp(current->next->lname, current->next->next->lname) > 0)
+            {
+                swapped = true;
+                positon temp = current->next;
+                current->next = current->next->next;
+                temp->next = current->next->next;
+                current->next->next = temp;
+            }
+            current = current->next;
+        }
+    }
+}*/
